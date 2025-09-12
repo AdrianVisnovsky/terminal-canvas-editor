225257a36f9d5d0e8ee5edce8014d32580c21348
Adrián Višňovský <adrian.visnovsky@protonmail.com>
2025-09-12 02:03:51 +0200

    Setting up main loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
-mod app;
-mod event;
-mod canvas;
-mod renderer;
-mod cursor;
-mod command;
+use std::io;
+use std::io::stdout;
+use crossterm::event::{read, KeyCode};
+use crossterm::{ExecutableCommand};
+use crossterm::cursor::{Hide, Show};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+
+    stdout()
+        .execute(EnterAlternateScreen)?
+        .execute(Hide)?;
+    enable_raw_mode()?;
+
+    while let Ok(event) = read() {
+
+        let Some(event) = event.as_key_event() else {
+            continue;
+        };
+
+        if event.code == KeyCode::Char('q') {
+            break;
+        }
+
+    }
+
+    disable_raw_mode()?;
+    stdout()
+        .execute(LeaveAlternateScreen)?
+        .execute(Show)?;
+
+    Ok(())
 }
